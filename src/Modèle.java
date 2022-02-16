97d0af34b43de4370033d4f17c8d323ca67fbb33
Valentin <valentin.merault@gmail.com>
2022-02-16 11:27:33 +0100

    Ajout de la classe Modèle

---
+++ src/Modèle.java
@@ -0,0 +1,12 @@
+import java.awt.Color;
+
+public class Modèle {
+	
+	private Color[] COULEURS = {Color.yellow, Color.GREEN, Color.blue, Color.MAGENTA,Color.RED, Color.orange, Color.WHITE, Color.black};
+	
+	private int N_TENTATIVES = 10;
+	
+	private int DIFFICULTE = 4;
+	
+	enum Etat { EN_COURS, GAGNE , PERDU}
+}
